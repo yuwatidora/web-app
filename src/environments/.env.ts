cfb9cab07d6fc173a7f9322dcd0b3eb45d67c73e
yuwatidora <yuwatinyi003@gmail.com>
2025-06-10 15:42:05 -0400

    tring out delete with angular

--- src/environments/.env.ts
+++ src/environments/.env.ts
@@ -2,8 +2,8 @@
 /* tslint:disable */
 export default {
   'mifos_x': {
-    'version': '250605',
-    'hash': '1d6b6fd7'
+    'version': '250609',
+    'hash': '7d741bb0'
   },
   'allow_switching_backend_instance': true
 };
